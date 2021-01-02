d3d3edf4fa1ee4115b69355f531e26ce1b783945
RootA <amwathi@fluidtechglobal.com>
2021-01-02 23:30:19 +0300

    confirming a blocks validity in the chain

--- chain.py
+++ chain.py
@@ -1,4 +1,11 @@
+from app import Block
+
 class Blockchain(object):
+    """
+    @chain - stores all the blocks
+    @current_data - stores all the info about the block
+    @build_genesis - creates the first block
+    """
     def __init__(self):
         self.chain = []
         self.current_data = []
@@ -6,14 +13,30 @@ class Blockchain(object):
         self.build_genesis()
 
     def build_genesis(self):
-        pass
+        self.build_block(proof_number=0, prev_hash=0)
     
-    def build_block(self):
-        pass
+    def build_block(self, proof_number, prev_hash):
+        block = Block(
+            index=len(self.chain),
+            proof_number=proof_number,
+            prev_hash=prev_hash,
+            data=self.current_data
+        )
+
+        self.current_data = []
+        self.chain.append(block)
+        return block
 
     @staticmethod
     def confirm_validity(block, prev_block):
-        pass
+        if prev_block.index + 1 != block.index:
+            return False
+        elif prev_block.compute_hash() != block.prev_hash:
+            return False
+        elif block.timestamp & lt; = prev_block.timestamp:
+            return False
+        
+        return True
 
     def get_data(self, sender, receiver, amount):
         pass
